4b98e3ea4a5d6282895be604994477241902bb3b
Enoch <enochhopemee@gmail.com>
2025-10-11 01:24:55 +0100

    my explanation on object oriented programming

---
+++ Custom_exception/enoch boy.py
@@ -0,0 +1,26 @@
+#object oriented programming,is one that has to do with classes and objects.
+#it has in it encapsulation,polymorphism,inheritance and methods.
+#example for classes and object
+
+def gender():
+    print("")
+
+g = 1
+print(type(gender))    
+
+#this would result into <class 'function'>
+#so if am to change that g to gender = boy
+#let me try it
+
+def gender():
+    print("boy")
+
+gender = "boy"
+print(type(gender))    
+
+#this would result to <class 'str'>
+#this for my definition on the basic concept on oop
+#Thank you
+
+
+
